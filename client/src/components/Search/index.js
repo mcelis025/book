48e614cff7f8941ac007bff4ef964bad3ca1655c
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-08-03 22:19:16 -0700

    switch class for classname

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -3,12 +3,12 @@ import React from "react";
 function Search() {
   return (
 
-    <div class="container mt-3">
-      <h4 class="mb-3">Book Search</h4>
-      <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Search"></input>
-        <div class="input-group-append">
-          <button class="btn btn-success" type="submit">Go</button>  
+    <div className="container mt-3">
+      <h4 className="mb-3">Book Search</h4>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Search"></input>
+        <div className="input-group-append">
+          <button className="btn btn-success" type="submit">Go</button>  
         </div>
       </div>
     </div>
