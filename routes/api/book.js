92d52439684631d80a05b117feb0d5c3806f2968
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-08-08 02:27:32 -0700

    require stack error

--- routes/api/book.js
+++ routes/api/book.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const bookController = require("../../controllers/bookController");
+const bookController = require("../../contollers/bookController");
 
 // Matches with "/api/books"
 router.route("/")
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-// const path = require("path");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,12 +12,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
